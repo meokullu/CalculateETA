8f6212b037a71014d02a7661f05b96a788a7edb5
meokullu <meokullu@gmail.com>
2024-09-01 16:04:31 +0300

    v3.0.2

--- CalculateETA/src/TextMessage.cs
+++ CalculateETA/src/TextMessage.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CalculateETA
+﻿namespace CalculateETA
 {
     public partial class CalculateETA
     {
